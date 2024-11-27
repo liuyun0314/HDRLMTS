3ffa99d7b9bda0aea59bc3ddc614529cdb7dfce8
Liu Yun <liuyun_8@163.com>
2024-11-27 16:27:34 +0800

    Update rewards.py

--- PPO/rewards.py
+++ PPO/rewards.py
@@ -92,7 +92,7 @@ def reward_idel_time(r, currentTime, machines):
     if r_next < r:
         reward = 1
     else:
-        if r_next == r:   # ref{Real-Time Scheduling for Dynamic Partial-No-Wait Multiobjective Flexible Job Shop by Deep Reinforcement Learning}
+        if r_next == r:   
             reward = 0
         else:
             reward = -1
@@ -105,7 +105,7 @@ def estimated_WT_mean(currentTime, jobs, machines):
     for m in machines:
         CKs.append(m.currentTime)
 
-    T_cure = np.mean(CKs)  # 机器的平均完成时间
+    T_cure = np.mean(CKs) 
     for j in jobs:
         if not j.completed:
             num_unfinished_jobs += 1
@@ -121,7 +121,7 @@ def estimated_WT_mean(currentTime, jobs, machines):
                     t_avg = np.mean(list(cMachines.values()))
                     all_t_avg.append(t_avg)
             sum_t_avg = sum(all_t_avg)
-            ETWT = j.weight * max(0, max(T_cure, C_last) + sum_t_avg - j.DT)
+            ETWT = j.weight * max(0, max(currentTime, C_last) + sum_t_avg - j.DT)
         else:
             ETWT = j.weight * max(0, j.endTime - j.DT)
         ETWTs.append(ETWT)
@@ -150,33 +150,12 @@ def estimated_WT_max(currentTime, jobs, machines):
                     t_avg = np.mean(list(cMachines.values()))
                     all_t_avg.append(t_avg)
             sum_t_avg = sum(all_t_avg)
-            ETWT = j.weight * max(0, max(T_cure, C_last) + sum_t_avg - j.DT)
+            ETWT = j.weight * max(0, max(currentTime, C_last) + sum_t_avg - j.DT)
         else:
             ETWT = j.weight * max(0, j.endTime - j.DT)
         ETWTs.append(ETWT)
     return np.max(ETWTs)
-
-def re(sim, last_Cmax):
-    Cmax = makespan(sim.machines)
-    gap = Cmax - last_Cmax   # gap不可能为负，因为生产时间只会越来越长；当gap为0时说明在该决策点处选择的任务没有被进一步处理
-    if gap == 0:
-        reward = 0
-    else:
-        reward = 1 / (Cmax - last_Cmax)
-    return reward
-
-
-def makespan(machines):
-    all_Cmax = []
-    for m in machines:
-        all_Cmax.append(m.currentTime)
-        # Cmax_m = 0
-        # for o in m.assignedOpera:
-        #     Cmax_m += o.duration
-        # all_Cmax.append(Cmax_m)
-    Cmax = max(all_Cmax)
-    return Cmax
-
+    
 def estimated_WF_mean(currentTime, jobs, machines):
     CKs = []
     ETWFs = []
@@ -201,44 +180,31 @@ def estimated_WF_mean(currentTime, jobs, machines):
                     t_avg = np.mean(list(cMachines.values()))
                     all_t_avg.append(t_avg)
             sum_t_avg = sum(all_t_avg)
-            ETWF = j.weight * (max(T_cure, C_last) + sum_t_avg - j.RT)
+            ETWF = j.weight * (max(currentTime, C_last) + sum_t_avg - j.RT)
         else:
             ETWF = j.weight * (j.endTime - j.RT)
         ETWFs.append(ETWF)
     return np.mean(ETWFs)
+    
+def re(sim, last_Cmax):
+    Cmax = makespan(sim.machines)
+    gap = Cmax - last_Cmax   # gap不可能为负，因为生产时间只会越来越长；当gap为0时说明在该决策点处选择的任务没有被进一步处理
+    if gap == 0:
+        reward = 0
+    else:
+        reward = 1 / (Cmax - last_Cmax)
+    return reward
 
-def machine_utilization(currentTime, machines):
-    UKs = []
+def makespan(machines):
+    all_Cmax = []
     for m in machines:
-        pt = 0
-        operations = m.assignedOpera
-        for o in operations:
-            if o.completed:
-                if o.endTime <= currentTime:
-                    pt += o.duration
-                else:
-                    pt += currentTime - o.startTime
-        if pt == 0 or currentTime == 0:
-            UK_m = 0
-        else:
-            UK_m = pt / currentTime
-        UKs.append(UK_m)
-    return np.mean(UKs)
-
-def machine_utilization_ratio(sim):
-    URs = []
-    Cmax = []
-    for ta in range(sim.num_tasks):
-        Cmax.append(sim.completed_jobs[ta][-1].endTime)
-    makespan = max(Cmax)
-    for m in sim.machines:
-        pt = 0
-        opera_list = m.assignedOpera
-        if len(opera_list):
-            for op in opera_list:
-                pt += op.duration
-        URs.append(pt / makespan)
-    return np.mean(URs)
+        all_Cmax.append(m.currentTime)
+        # Cmax_m = 0
+        # for o in m.assignedOpera:
+        #     Cmax_m += o.duration
+        # all_Cmax.append(Cmax_m)
+    Cmax = max(all_Cmax)
+    return Cmax
 
 def rewardGlobal(obj, r_MR, sim):
     r_WT_mean = obj[0]
@@ -262,29 +228,6 @@ def rewardGlobal(obj, r_MR, sim):
                 reward = -3
     return reward
 
-# def reward_global(obj, r_MR, sim):
-#     r_WT_mean = obj[0]
-#     r_WT_max = obj[1]
-#     r_WF_mean = obj[2]
-#
-#     objectives = three_objection(sim)
-#     r_WT_mean_next = objectives[0]
-#     r_WT_max_next = objectives[1]
-#     r_WF_mean_next = objectives[2]
-#     r_MR_next = machine_utilizatioin_ratio(sim)
-#     if r_WT_mean_next < r_WT_mean and r_WT_max_next < r_WT_max and r_WF_mean_next < r_WF_mean:
-#         reward = 3
-#     else:
-#         if (r_WT_mean_next < r_WT_mean and r_WT_max_next < r_WT_max) or (r_WT_mean_next < r_WT_mean and r_WF_mean_next < r_WF_mean) or (r_WT_max_next < r_WT_max and r_WF_mean_next < r_WF_mean):
-#             reward = 2
-#         else:
-#             if r_WT_mean_next < r_WT_mean or r_WT_max_next < r_WT_max or r_WF_mean_next < r_WF_mean:
-#                 reward = 1
-#             else:
-#                 reward = -3
-#
-#     return reward
-
 def reward_global(obj, r_MR, sim):
     r_WT_mean = obj[0]
     r_WT_max = obj[1]
@@ -294,20 +237,17 @@ def reward_global(obj, r_MR, sim):
     r_WT_mean_next = objectives[0]
     r_WT_max_next = objectives[1]
     r_WF_mean_next = objectives[2]
-    r_MR_next = machine_utilizatioin_ratio(sim)
-    if r_MR_next >= r_MR:
-        if r_WT_mean_next < r_WT_mean and r_WT_max_next < r_WT_max and r_WF_mean_next < r_WF_mean:
-            reward = 3
-        else:
-            if (r_WT_mean_next < r_WT_mean and r_WT_max_next < r_WT_max) or (r_WT_mean_next < r_WT_mean and r_WF_mean_next < r_WF_mean) or (r_WT_max_next < r_WT_max and r_WF_mean_next < r_WF_mean):
-                reward = 2
-            else:
-                if r_WT_mean_next < r_WT_mean or r_WT_max_next < r_WT_max or r_WF_mean_next < r_WF_mean:
-                    reward = 1
-                else:
-                    reward = -1
+    if r_WT_mean_next < r_WT_mean and r_WT_max_next < r_WT_max and r_WF_mean_next < r_WF_mean:
+        reward = 3
     else:
-        reward = -1
+        if (r_WT_mean_next < r_WT_mean and r_WT_max_next < r_WT_max) or (r_WT_mean_next < r_WT_mean and r_WF_mean_next < r_WF_mean) or (r_WT_max_next < r_WT_max and r_WF_mean_next < r_WF_mean):
+            reward = 2
+        else:
+            if r_WT_mean_next < r_WT_mean or r_WT_max_next < r_WT_max or r_WF_mean_next < r_WF_mean:
+                reward = 1
+            else:
+                reward = -3
+    reward = reward - (-3) / (3 - (-3))
     return reward
 
 def highLevel_reward(sim, last_Cmax, obj):
@@ -335,12 +275,9 @@ def highLevel_reward(sim, last_Cmax, obj):
                 reward = 1
             else:
                 reward = -3
-    reward = reward - (-1) / (3 - (-1)) # 为避免奖励函数倾斜，将奖励函数缩放到0到1之间 （x - min）/ (max - min)
+    reward = reward - (-3) / (3 - (-3))
     reward = reward + (Cmax - last_Cmax) / last_Cmax
     return reward
-
-
-
 
 def reward_PPO_withoutMultitask(r_WT_mean, r_WT_max, r_WF_mean, sim):
     objectives = three_objection(sim)
@@ -409,39 +346,7 @@ def reward_WF_mean1(r, jobs, machines, currentTime):
             return 0
         else:
             return 1 / (r_next - r)
-
-def reward_global1(r, sim):
-
-    r_next = machine_utilizatioin_ratio(sim)
-    if r_next > r:
-        reward = 1
-    else:
-        if r_next > 0.9 * r:
-            reward = 0
-        else:
-            reward = 1
-    return reward
-    # return r_next - r
-    # if r_next > r:
-    #     return 1
-    # else:
-    #     if r_next == r:
-    #         return 0
-    #     else:
-    #         return r_next - r
-
-def Controller_Reward(r, reward_task, currentTime, machines):
-    UR_next = machine_utilization(currentTime, machines)
-    if UR_next > r:
-        reward_UR = 1
-    else:
-        if UR_next >= 0.97 * r:  # ref{Real-Time Scheduling for Dynamic Partial-No-Wait Multiobjective Flexible Job Shop by Deep Reinforcement Learning}
-            reward_UR = 0
-        else:
-            reward_UR = -1
-    reward = reward_UR + reward_task[0] + reward_task[1] + reward_task[2]
-    return reward
-
+            
 def three_objection(sim):
     EWT_mean = estimated_WT_mean(sim.env.now, sim.tasks_list[0].jobsList, sim.machines)
     EWT_max = estimated_WT_max(sim.env.now, sim.tasks_list[1].jobsList, sim.machines)
@@ -502,40 +407,6 @@ def WF_mean_func(jobs):
         WF = j.weight * max(0, j.endTime - j.RT)
         WFs.append(WF)
     return np.mean(WFs)
-
-def machine_utilizatioin_ratio(sim):
-    Cmax = []
-    PTs = []
-    for m in sim.machines:
-        Cmax.append(m.currentTime)
-        pt = 0
-        for o in m.assignedOpera:
-            pt += o.duration
-        PTs.append(pt)
-    makespan = max(Cmax)
-    if makespan == 0:
-        return 0
-    else:
-        MR = [i/makespan for i in PTs]
-        return np.mean(MR)
-
-def global_Reward(r, pre_nas, currentTime, machines):
-    UR_next = machine_utilization(currentTime, machines)
-
-    if UR_next > r:
-        reward_UR = 1
-    else:
-        if UR_next >= 0.90 * r:  # ref{Real-Time Scheduling for Dynamic Partial-No-Wait Multiobjective Flexible Job Shop by Deep Reinforcement Learning}
-            reward_UR = 0
-        else:
-            reward_UR = -1
-    num_all_assigned = num_assigned_jobs(machines)
-    if pre_nas < num_all_assigned:
-        reward_op = -10
-    else:
-        reward_op = num_all_assigned - pre_nas
-    reward = reward_UR + reward_op
-    return reward
 
 def num_assigned_jobs(machines):
     num_all_assigned = 0
